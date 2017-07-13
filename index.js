afa7cd175f75686bc65f1c5d9132b11fae0e6be8
ssmak <steave.mkm@hotmail.com>
2017-07-13 17:35:37 +0800

    Period commit.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const moment = require('moment');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
 app.use(express.static(path.join(__dirname, '/')));
 app.get('/', function (req, res) {
@@ -24,8 +27,48 @@ io.on('connection', function (socket) {
 	socket.on('play', function (cmd) {
 		console.log('receive remote cmmand: ');
 		console.log(cmd);
-		
+				
 		socket.broadcast.emit('play', cmd);
+	});
+	
+	socket.on('download', function (cmd) {
+		console.log('download the resource: ');
+		console.log(cmd);
+		
+		let download_folder = __dirname + '/download/' + moment().format('YYYYMMDD');
+		if(!fs.existsSync(__dirname + '/download')) {
+			fs.mkdirSync(__dirname + '/download');
+		}
+		if(!fs.existsSync(download_folder)) {
+			fs.mkdirSync(download_folder);
+		}
+		let filename = cmd.resource_title.replace("'", '').replace('"', '').replace('.', '').replace('\\', '').replace('/', '');
+		let output_stream = fs.createWriteStream(download_folder + '/' + filename + '.mp4');
+		output_stream.on('error', function (err) {
+			console.log("output file stream error: ");
+			console.log(err);
+		});		
+		(
+			function (filename) {
+				output_stream.on('finish', function () {
+					console.log("complete download");
+					console.log(filename);
+					socket.emit('system', {
+						type: 'download_completed',
+						filename: filename
+					});
+				});
+			}
+		)(filename);
+		let video = ytdl('http://www.youtube.com/watch?v=' + cmd.resource_id);
+		video.pipe(output_stream);
+	});
+	
+	socket.on('error', function (err) {
+		console.log('socket err: ');
+		console.log(err);
+		
+		socket.disconnect();
 	});
 });
 
