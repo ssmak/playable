5f7d84acf5911969fe61270db467755ea9bae9ac
Steve Mak <steave.mkm@hotmail.com>
2019-05-10 10:27:55 +0800

    Period commit.

--- resource-loader.js
+++ resource-loader.js
@@ -1,7 +1,7 @@
 var ys = require('youtube-search');
 var opts = {
 	maxResults: 10,
-	key: 'AIzaSyDLjs_Am0ea21WvJTouKYOBbmtA5_SECwk'
+	key: 'AIzaSyDHTuBJeubsoh8-cjVQEcGrZ9UOV_1uS5Y'
 }
 
 var search = function (keyword, limit, page_token) {
